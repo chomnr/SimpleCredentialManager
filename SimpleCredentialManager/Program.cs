42c56da3f78df775750f0313ae2d270ba4ca397a
chomnr <zeljkovranjes@outlook.com>
2023-07-24 15:08:49 -0400

    added what i need to do.

--- SimpleCredentialManager/Program.cs
+++ SimpleCredentialManager/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// aes encryption
+
+
+// create new key / storage
+// import key & storage
+// search functionality search for specific credential
+// delete value
+// values are in json format and then encrypted with aes ending with .scm (simple credential manager)
+
+// First screen
+// [1] = Create new key / storage (exports key after creation) & storage
+// [2] = Import existing key / storage
+
+// Second screen 
+// [1] Create new Credential
+// [2] Delete Credential
+// [3] Update Credential 
+// [4] Search Credential
+// Changes are automatically saved after the selected action is finished
